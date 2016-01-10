5332f69e0f653eb21e8cae33c03c188d8caba996
Mitesh Shah <miteshninja@gmail.com>
2016-01-10 18:27:12 +0530

    Fixed front page + metapost layout. git rm'd _site

--- _site/README.md
+++
@@ -1,1 +0,0 @@
-My personal site: [miteshshah.com](http://miteshshah.com)
--- _site/addCategory.sh
+++
@@ -1,7 +0,0 @@
-slug=$1
-title=$2
-
-echo "\n- slug: $slug" >> _data/categories.yml
-echo "  name: $slug" >> _data/categories.yml
-
-echo "---\nlayout: category_layout\ntitle: \"$title\"\ncategory: $slug\npermalink: /$slug/\n---" > blog/category/${slug}.md
